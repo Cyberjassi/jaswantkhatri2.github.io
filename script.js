26d643bb683019b831f0f15139100bb1901f8c8c
Cyberjassi <jaswantkhatri30@gmail.com>
2022-12-05 22:32:40 +0530

    update

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Developer","Player" , "Servant", "Learner", "Student"],
+        strings: ["Developer","Player" , "Learner", "Student"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
